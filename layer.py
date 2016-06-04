b1b351d54722fca165d5173989034612b069dfef
tjacek <tjacek@student.agh.edu.pl>
2016-06-04 22:39:27 +0200

    recurent layer

--- layer.py
+++ layer.py
@@ -21,3 +21,27 @@ class Layer(object):
         else:
             return self.activation(
                 T.dot(self.linear_matrix, x) + self.bias_matrix )
+
+class RNN(Layer):
+    def __init__(self, *args, **kwargs):
+        super(RNN, self).__init__(*args, **kwargs)
+        self.is_recursive = True
+
+    def create_variables(self):
+        self.linear_matrix        = tools.create_shared(self.hidden_size, self.input_size+ self.hidden_size, name="RNN.linear_matrix")
+        self.bias_matrix          = tools.create_shared(self.hidden_size, name="RNN.bias_matrix")
+        self.initial_hidden_state = tools.create_shared(self.hidden_size, name="RNN.initial_hidden_state")
+
+    def activate(self, x, h):
+        if x.ndim > 1:
+            return self.activation(
+                T.dot(
+                    self.linear_matrix,
+                    T.concatenate([x, h], axis=1).T
+                ) + self.bias_matrix[:,None] ).T
+        else:
+            return self.activation(
+                T.dot(
+                    self.linear_matrix,
+                    T.concatenate([x, h])
+                ) + self.bias_matrix )
