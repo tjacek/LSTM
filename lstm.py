cfb991ad4e666117254a9da9231df45ec30ea1ba
tjacek <tjacek@student.agh.edu.pl>
2016-06-14 21:27:39 +0200

    lstm implemented

--- lstm.py
+++ lstm.py
@@ -4,10 +4,16 @@ import tools
 
 def mask_lstm(hyper_params,in_var,mask_var):
     #U, V, W = init_params(hyper_params)
-    def recurrence(x_t,h_state,cell_state):
-        #h_t = T.nnet.sigmoid( U.linear(x_t) + V.linear(h_state) )
-        #s_t = T.nnet.softmax(W.linear(h_t))
-        return [h_t[0],s_t]
+    forget_gate,in_gate,cell_gate,out_gate=lstm_params(hyper_params)
+    def recurrence(x_t1,h_t,c_t):
+        xh_t=T.concatenate([x_t1,h_t])
+        f_t=T.nnet.sigmoid(forget_gate.linear(xh_t))
+        i_t=T.nnet.sigmoid(in_gate.linear(xh_t))
+        c_t_prop=T.nnet.tanh(cell_gate.linear(xh_t))
+        c_t_next=f_t*c_t+i_t*c_t_prop
+        o_t=T.nnet.sigmoid(out_gate.linear(xh_t))
+        h_t_next=o_t*T.nnet.tanh(c_t_next)
+        return [h_t_next,c_t_next]
 
     [h,s], updates = theano.scan(
             recurrence,
