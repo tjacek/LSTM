56001f57c16ad2d1e63dd74004d060bcde9437ee
tjacek <tjacek@student.agh.edu.pl>
2016-06-23 13:54:48 +0200

    get params values

--- rnn.py
+++ rnn.py
@@ -6,14 +6,23 @@ import lstm
 
 class RNN(object):
     def __init__(self,hyper_params,
-    	              nn_vars,
+    	              params,nn_vars,
     	              pred,loss,updates):
         in_var=nn_vars['in_var']
         target_var=nn_vars['target_var']
+        #print(dir(params[0]))
+        self.params=params
         self.pred=theano.function([in_var], pred,allow_input_downcast=True,on_unused_input='warn')        
         self.loss=theano.function([in_var,target_var], loss,allow_input_downcast=True)
         self.updates=theano.function([in_var, target_var], loss, 
                                updates=updates,allow_input_downcast=True)
+        self.get_params()
+
+    def get_params(self):
+        param_value=[ (param_i.name,param_i.get_value()) 
+                 for param_i in self.params]
+        #print(names)
+        return dict(param_value)
 
 class MaskRNN(object):
     def __init__(self,hyper_params,
@@ -36,14 +45,15 @@ class TestRNN(object):
         self.pred=theano.function([in_var,mask_var,target_var], pred,allow_input_downcast=True,on_unused_input='warn')     
 
 def build_rnn(hyper_params):
-    nn_vars=init_variables(True)
-    builder=lstm.MaskLSTMBuilder(hyper_params,True)
+    nn_vars=init_variables(False)
+    builder=lstm.LSTMBuilder(hyper_params,True)
     hidden=builder.get_output(nn_vars)
     params=builder.get_params()
     #pred_y,loss=regresion(hidden,nn_vars['target_var'])
     pred_y,loss=prob(hidden,nn_vars['target_var'],hyper_params,params)
+    
     updates=optim.momentum_sgd(loss,hyper_params,params)
-    return MaskRNN(hyper_params,nn_vars,pred_y,loss,updates)
+    return RNN(hyper_params,params,nn_vars,pred_y,loss,updates)
 
 def regresion(hidden,target_var):
     pred_y= T.mean(T.mean(hidden,axis=0),axis=1)
@@ -54,15 +64,21 @@ def regresion(hidden,target_var):
 
 def prob(hidden,y,hyper_params,params):
     p_x=hidden
-    y_hot=T.extra_ops.to_one_hot(y,p_x.shape[2])
-    y_full=T.tile(y_hot, (p_x.shape[0],1,1) )
-    loss=T.sum(T.nnet.categorical_crossentropy( p_x,y_full))#,loss,updates
+    #ret = theano.tensor.zeros((y.shape[0],y.shape[1], p_x.shape[2]),
+    #                          dtype=p_x.dtype)
+    #ret = theano.tensor.set_subtensor(ret[theano.tensor.arange(y.shape[0]),
+    #                                      theano.tensor.arange(y.shape[1]), y],
+    #                                  1)
+    #loss=ret
+    #y_hot=T.extra_ops.to_one_hot(y,p_x.shape[2])
+    #y_full=T.tile(y_hot, (p_x.shape[0],1,1) )
+    loss=T.mean(T.nnet.categorical_crossentropy( p_x,y))
     return p_x,loss
 
 def init_variables(mask_var=False):
     nn_vars={}
     nn_vars['in_var'] = T.ltensor3('in_var')
-    nn_vars['target_var'] = T.ivector('target_var')
+    nn_vars['target_var'] = T.ltensor3('target_var')
     if(mask_var):
         nn_vars['mask_var']= T.lmatrix('mask_var')
     return nn_vars	
