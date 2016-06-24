c7989410529f6c11a4949018614086a5aab12b44
tjacek <tjacek@student.agh.edu.pl>
2016-06-24 14:34:23 +0200

    default args

--- char_nn.py
+++ char_nn.py
@@ -8,7 +8,7 @@ def generate_note(n,seed='polska'):
     note=char.seq_to_words(seq_m)
     return note
 
-def read_lstm(filename='models/lstm'):
+def read_lstm(filename='models/lstm300'):
     n_chars=len(char.ALPHA)
     pap_params={'n_cats':n_chars,'seq_dim':n_chars,'hidden_dim':3*n_chars,
             'cell_dim':3*n_chars,'learning_rate':0.1,'momentum':0.9}
@@ -17,6 +17,12 @@ def read_lstm(filename='models/lstm'):
     return rnn_model
 
 if __name__ == "__main__":
-    n=int(sys.argv[1])
-    seed=sys.argv[2]
+    if(len(sys.argv)<2):
+        n=100
+    else:
+        n=int(sys.argv[1])
+    if(len(sys.argv)<2):
+        seed=u'polska'
+    else:
+        seed=sys.argv[2]
     print(generate_note(n,seed))
