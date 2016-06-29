ba46638ec3ec38a079d681d4476031d6afa5c8be
tjacek <tjacek@student.agh.edu.pl>
2016-06-29 13:55:17 +0200

    generate sequences

--- gen_seq.py
+++ gen_seq.py
@@ -10,13 +10,12 @@ def gen_seq(size,model,chars=None):
     return np.array(chars)  
 
 def gen_next(char_prev,model):
-    #char_prev=char_as_vec(k)
     prob=model.pred(char_prev)[-1]
-    if(is_space(char_prev)):     
-        k_next=random_char(prob)
+    if(is_space_k(char_prev,k=2)):     
+        char_next=sample_next_char(prob)
     else:
-        k_next=determ_next_char(prob)
-    return char_as_vec(k_next)
+        char_next=determ_next_char(prob)
+    return char_as_vec(char_next)
 
 def determ_next_char(prob,n_cats=36):
     prob=prob.flatten()
@@ -30,7 +29,6 @@ def random_char(prob,n_cats=20):
     return np.random.randint(n_cats)+1
 
 def char_as_vec(k,n_cats=36):
-    #char_shape[1]=n_cats
     seed=np.zeros((1,n_cats),dtype=float)
     seed[0][k]=1.0
     return seed
@@ -38,3 +36,12 @@ def char_as_vec(k,n_cats=36):
 def is_space(char_prev):
     last_char=char_prev[-1].flatten()
     return last_char[0]==1.0
+
+def is_space_k(char_prev,k=2):
+    if(char_prev.shape[0]<k):
+        return False
+    for i in range(k):
+        prev_char_i=char_prev[-(i+1)].flatten() 
+        if(prev_char_i[0]==1.0):
+            return True    
+    return False
